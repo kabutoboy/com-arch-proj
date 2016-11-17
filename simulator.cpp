8a855ca87a908da84129c8e5e3ca650b540690c0
Woratham Kh <kabuto.boy@gmail.com>
2016-11-17 20:20:07 +0700

    refine to a bit

--- simulator.cpp
+++ simulator.cpp
@@ -11,59 +11,86 @@ vector<int32_t> reg(regSize, 0);
 vector<int32_t> mem(memSize, 0);
 vector<int32_t> ins;
 
-int32_t maskOpcode = 0b111 << 22;
-int32_t maskRegA = 0b111 << 19;
-int32_t maskRegB = 0b111 << 16;
-int32_t maskDestReg = 0b111;
+int32_t maskOpcode      = 0b111 << 22;
+int32_t maskRegA        = 0b111 << 19;
+int32_t maskRegB        = 0b111 << 16;
+int32_t maskDestReg     = 0b111;
 int32_t maskOffsetField = 0b1111111111111111;
 
 int8_t getOpcode(int32_t i) {
+
     return (i & maskOpcode) >> 22;
+
 }
 
 int8_t getRegA(int32_t i) {
+
     return (i & maskRegA) >> 19;
+
 }
 
 int8_t getRegB(int32_t i) {
+
     return (i & maskRegB) >> 16;
+
 }
 
 int8_t getDestReg(int32_t i) {
+
     return (i & maskDestReg);
+
 }
 
 int16_t getOffsetField(int32_t i) {
+
     return (i & maskOffsetField);
+
 }
 
 void getInstructions() {
+
     ins.clear();
     int32_t i;
     int j = 0;
+
     while (cin >> i) {
-	ins.push_back(i);
-	mem[j] = i;
-	j++;
+
+        ins.push_back(i);
+        mem[j] = i;
+        j++;
+
     }
+
 }
 
 void printState() {
+
     printf("@@@\n");
     printf("state:\n");
     printf("\tpc %d\n", pc);
+
     //printf("\tmem: \n");
+    //
     //for (int j = 0; j < ins.size(); j++) {
+    //
     //    printf("\t\tmem[%d] %d\n", j, mem[j]);
+    //
     //}
+
     printf("\tregisters: \n");
+
     for (int j = 0; j < regSize; j++) {
-	printf("\t\treg[%d] %d\n", j, reg[j]);
+
+        printf("\t\treg[%d] %d\n", j, reg[j]);
+        
     }
+
     printf("end state\n");
+
 }
 
 int main() {
+
     getInstructions();
 
     /*
@@ -76,72 +103,127 @@ int main() {
      * $6 temp
      * $7 return addr
      */
-    //reg[5] = memSize-1;
-    int totalIns = 0;
+
     pc = 0;
-    bool halt = false;
     int32_t i;
+    bool halt = false;
+    int totalIns = 0;
+
     while (!halt) {
-	totalIns += 1;
-	i = ins[pc];
-	printState();
-	pc += 1;
-	int8_t opcode = getOpcode(i),
-	       regA = getRegA(i),
-	       regB = getRegB(i),
-	       destReg = getDestReg(i);
-	int32_t offsetField = getOffsetField(i);
-	switch (opcode) {
-	    case 0:
-		// add (r-type)
-		printf("add\t%d\t%d\t%d\n", regA, regB, destReg);
-		reg[destReg] = reg[regA] + reg[regB];
-		break;
-	    case 1:
-		// nand (r-type)
-		printf("nand\t%d\t%d\t%d\n", regA, regB, destReg);
-		reg[destReg] = ~(reg[regA] & reg[regB]);
-		break;
-	    case 2:
-		// lw (i-type)
-		printf("lw\t%d\t%d\t%d\n", regA, regB, offsetField);
-		reg[regB] = mem[reg[regA] + offsetField];
-		break;
-	    case 3:
-		// sw (i-type)
-		printf("sw\t%d\t%d\t%d\n", regA, regB, offsetField);
-		mem[reg[regA] + offsetField] = reg[regB];
-		break;
-	    case 4:
-		// beq (i-type)
-		printf("beq\t%d\t%d\t%d\n", regA, regB, offsetField);
-		if (reg[regA] == reg[regB]) {
-		    pc += offsetField;
-		}
-		break;
-	    case 5:
-		// jalr (j-type)
-		printf("jalr\t%d\t%d\n", regA, regB);
-		reg[regB] = pc;
-		pc = reg[regA];
-		break;
-	    case 6:
-		// halt (o-type)
-		printf("halt\n");
-		halt = true;
-		break;
-	    case 7:
-		// noop (o-type)
-		printf("noop\n");
-		break;
-	    default:
-		
-		break;
-	}
-	if (halt) {
-	    printState();
-	    break;
-	}
+
+        totalIns += 1;
+        i = ins[pc];
+
+        printState();
+
+        pc += 1;
+
+        int8_t opcode   = getOpcode(i),
+               a        = getRegA(i),
+               b        = getRegB(i),
+               dest     = getDestReg(i);
+        int32_t offset  = getOffsetField(i);
+
+        printf("\n");
+        
+        switch (opcode) {
+
+            // add (r-type)
+            case 0:
+
+                printf("add\t%d\t%d\t%d\n", a, b, dest);
+
+                reg[dest] = reg[a] + reg[b];
+
+                break;
+
+            // nand (r-type)
+            case 1:
+
+                printf("nand\t%d\t%d\t%d\n", a, b, dest);
+
+                reg[dest] = ~(reg[a] & reg[b]);
+
+                break;
+
+            // lw (i-type)
+            case 2:
+
+                printf("lw\t%d\t%d\t%d\n", a, b, offset);
+
+                reg[b] = mem[reg[a] + offset];
+
+                break;
+
+            // sw (i-type)
+            case 3:
+
+                printf("sw\t%d\t%d\t%d\n", a, b, offset);
+
+                mem[reg[a] + offset] = reg[b];
+
+                break;
+
+            // beq (i-type)
+            case 4:
+
+                printf("beq\t%d\t%d\t%d\n", a, b, offset);
+
+                if (reg[a] == reg[b]) {
+                    pc += offset;
+                }
+
+                break;
+
+            // jalr (j-type)
+            case 5:
+
+                printf("jalr\t%d\t%d\n", a, b);
+
+                reg[b] = pc;
+                pc = reg[a];
+
+                break;
+
+            // halt (o-type)
+            case 6:
+
+                printf("halt\n");
+
+                halt = true;
+
+                break;
+
+            // noop (o-type)
+            case 7:
+
+                printf("noop\n");
+
+                break;
+
+            // this should not happen
+            default:
+
+                printf("unrecognized opcode %d\n", opcode);
+                
+                break;
+
+        }
+
+        printf("\n");
+
+        if (halt) {
+
+            printState();
+
+            break;
+
+        }
     }
+
+    printf("\n");
     printf("totalIns %d\n", totalIns);
+
+    return 0;
+
 }
