99c451fb4aee6a2da07b333a3aad3ae709124c10
Woratham Kh <kabuto.boy@gmail.com>
2016-11-17 17:17:43 +0700

    combination fix

--- combination.s
+++ combination.s
@@ -1,29 +1,42 @@
+main	lw	0	1	N	
+	lw	0	2	R	
+	lw	0	5	sp	
+	lw	0	4	caddr	
+	jalr	4	7		
+	halt
+	noop
 combi	beq	2	0	base	
 	beq	1	2	base	
-	lw	0	6	neg8	
-	add	5	5	6	
-	sw	5	1	0	
-	sw	5	2	4	
+	lw	0	6	neg3	
+	add	6	5	5	
+	sw	5	7	0
+	sw	5	1	1	
+	sw	5	2	2	
 	lw	0	6	neg1	
-	add	1	1	6	
-	lw	0	4	combi	
+	add	6	1	1	
+	lw	0	4	caddr	
 	jalr	4	7		
-	lw	5	1	0	
-	lw	5	2	4	
+	lw	5	1	1
+	lw	5	2	2	
 	lw	0	6	neg1	
-	add	1	1	6	
-	add	2	2	6	
-	lw	0	4	combi	
+	add	6	1	1	
+	add	6	2	2	
+	lw	0	4	caddr	
 	jalr	4	7		
-	lw	0	6	pos8	
-	add	5	5	6	
+	lw	5	7	0
+	lw	0	6	pos3
+	add	6	5	5	
 	jalr	7	6		
 	noop
 base	lw	0	6	pos1	
-	add	3	3	6	
+	add	6	3	3	
 	jalr	7	6		
 	noop
+N	.fill	7
+R	.fill	3
+sp	.fill	65536
+caddr	.fill	combi
 pos1	.fill	1
-pos8	.fill	8
+pos3	.fill	3
 neg1	.fill	-1
-neg8	.fill	-8
+neg3	.fill	-3
--- combination.withnames.s
+++ combination.withnames.s
@@ -1,28 +1,38 @@
+main	lw	0	tmp	N	tmp = n
+	add	0	tmp	n	n = tmp
+	lw	0	tmp	R	tmp = r
+	add	0	tmp	r	r = tmp
+	lw	0	var	combi	var = combi.addr
+	jalr	var	tmp		tmp = pc+1; j var
+	halt
+	noop
 combi	beq	r	0	base	test r == 0
 	beq	n	r	base	test n == r
 	lw	0	tmp	neg8	tmp = -8
-	add	st	st	tmp	st = st + tmp
+	add	tmp	st	st	st = -8 + st
 	sw	st	n	0	mem[st + 0] = n
 	sw	st	r	4	mem[st + 4] = r
 	lw	0	tmp	neg1	tmp = -1
-	add	n	n	tmp	n = n - 1
+	add	tmp	n	n	n = -1 + n
 	lw	0	var	combi	var = combi.addr
 	jalr	var	ra		ra = pc+1; j var
 	lw	st	n	0	n = mem[st + 0]
 	lw	st	r	4	r = mem[st + 4]
 	lw	0	tmp	neg1	tmp = -1
-	add	n	n	tmp	n = n - 1
-	add	r	r	tmp	r = r - 1
+	add	tmp	n	n	n = -1 + n
+	add	tmp	r	r	r = -1 + r
 	lw	0	var	combi	var = combi.addr
 	jalr	var	ra		ra = pc+1; j var
-	lw	0	tmp	pos8	tmp = +8
-	add	st	st	tmp	st = st + tmp
+	lw	0	tmp	pos8	tmp = 8
+	add	tmp	st	st	st = 8 + st
 	jalr	ra	tmp		tmp = pc+1; j ra
-	
-base	lw	0	tmp	pos1	tmp = +1
-	add	ret	ret	tmp	ret = ret + 1
+	noop
+base	lw	0	tmp	pos1	tmp = 1
+	add	tmp	ret	ret	ret = 1 + ret
 	jalr	ra	tmp		tmp = pc+1; j ra
-
+	noop
+N	.fill	10
+R	.fill	5
 pos1	.fill	1
 pos8	.fill	8
 neg1	.fill	-1
